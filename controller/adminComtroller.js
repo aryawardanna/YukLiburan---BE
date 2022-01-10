a41899c2b2209af0311d75dd645a206116c9d706
aryawardanna <aryamadridfc@gmail.com>
2022-01-10 21:45:47 +0800

    delete feature itemID

--- controller/adminComtroller.js
+++ controller/adminComtroller.js
@@ -422,6 +422,30 @@ module.exports = {
     }
   },
 
+  deleteFeature: async (req, res) => {
+    const { id, itemId } = req.params;
+    try {
+      const feature = await Feature.findOne({ _id: id });
+
+      const item = await Item.findOne({ _id: itemId }).populate('featureId');
+      for (let i = 0; i < item.featureId.length; i++) {
+        if (item.featureId[i]._id.toString() === feature._id.toString()) {
+          item.featureId.pull({ _id: feature._id });
+          await item.save();
+        }
+      }
+      await fs.unlink(path.join(`public/${feature.imageUrl}`));
+      await feature.remove();
+      req.flash('alertMessage', 'Success Delete Feature');
+      req.flash('alertStatus', 'danger');
+      res.redirect(`/admin/item/show-detail-item/${itemId}`);
+    } catch (error) {
+      req.flash('alertMessage', `${error.message}`);
+      req.flash('alertStatus', 'danger');
+      res.redirect(`/admin/item/show-detail-item/${itemId}`);
+    }
+  },
+
   viewBooking: (req, res) => {
     res.render('admin/booking/view_booking', {
       title: 'LiburanYuk! | Booking',
